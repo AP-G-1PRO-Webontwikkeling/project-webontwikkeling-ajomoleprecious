903765fe401638777a1c4b21446e062421a62955
ajomole_precious <precious.ajomole@student.kdg.be>
2024-02-22 18:59:23 +0000

    wrote function to race to get data

--- Pokemons/index.ts
+++ Pokemons/index.ts
@@ -1,13 +1,23 @@
-import readline from 'readline-sync';
+//import readline from 'readline-sync';
+import pokemonsFromJson from '../Pokemon.json';
 
-let pokemons = fetch('https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-ajomoleprecious/main/Pokemon.json').then(response => response.json());
+let pokemonsFromFetch = fetch('https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-ajomoleprecious/main/Pokemon.json').then(response => response.json());
 
+const fetchFromUrl = async () => {
+    const response = await fetch('https://raw.githubusercontent.com/AP-G-1PRO-Webontwikkeling/project-webontwikkeling-ajomoleprecious/main/Pokemon.json');
+    const data = await response.json();
+    return data;
+}
 
-function showPokemon(data : any) {
+const fetchFromFile = async () => {
+    return pokemonsFromFetch;
+}
+
+function showPokemon(data: any) {
     console.log(data);
 }
-pokemons.then(data => {
+
+Promise.race([fetchFromUrl(), fetchFromFile()]).then(data => {
     showPokemon(data);
 });
-
 
